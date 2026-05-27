018ce18edce1aafb03914b45508e0ede865f573e
Davilapeixoto <davilapeixoto52@gmail.com>
2026-05-27 01:30:03 -0300

    Textura tapioca

--- src/games/cozinhaDeTapioca.c
+++ src/games/cozinhaDeTapioca.c
@@ -12,76 +12,107 @@ typedef struct {
     float x, y;
     float w, h;
     char nome[32];
-    Color cor;
     int colocado;
+    Texture2D tex;
 } Ingrediente;
+
+static void DrawIngrediente(Ingrediente *ing) {
+    DrawRectangleRounded((Rectangle){ing->x-6,ing->y-6,ing->w+12,ing->h+12},0.3f,4,(Color){255,245,220,160});
+    DrawTexturePro(ing->tex,(Rectangle){0,0,(float)ing->tex.width,(float)ing->tex.height},(Rectangle){ing->x,ing->y,ing->w,ing->h},(Vector2){0,0},0.0f,WHITE);
+    int lw=MeasureText(ing->nome,14);
+    DrawText(ing->nome,(int)(ing->x+ing->w/2.0f)-lw/2,(int)(ing->y+ing->h+5),14,DARKBROWN);
+}
 
 void cozinhaDeTapioca() {
     int screenWidth=1200;
     int screenHeight=720;
-
     SetTargetFPS(60);
-
     int flag=0;
     int errou=0;
     int frames=120;
-
-    Ingrediente ingredientes[6]={
-        {900,150,900,150,160,60,"Queijo",YELLOW,0},
-        {900,240,900,240,160,60,"Presunto",(Color){255,150,150,255},0},
-        {900,330,900,330,160,60,"Coco",(Color){240,240,220,255},0},
-        {900,420,900,420,160,60,"Banana",(Color){255,230,80,255},0},
-        {900,510,900,510,160,60,"Frango",ORANGE,0},
-        {900,600,900,600,160,60,"Doce de leite",(Color){180,100,40,255},0}
+    Texture2D texFundo=LoadTexture("../assets/sprites/cozinhaDeTapioca/Tapioca_com_tapioca.png");
+    Texture2D texTapioca=LoadTexture("../assets/sprites/cozinhaDeTapioca/tapioca_ingrediente.png");
+    Texture2D texCarne=LoadTexture("../assets/sprites/cozinhaDeTapioca/carne.png");
+    Texture2D texPeixe=LoadTexture("../assets/sprites/cozinhaDeTapioca/peixe.png");
+    Texture2D texAlface=LoadTexture("../assets/sprites/cozinhaDeTapioca/alface.png");
+    Rectangle srcFundo={0,0,(float)texFundo.width,(float)texFundo.height};
+    Rectangle dstFundo={0,0,(float)screenWidth,(float)screenHeight};
+    float tapScale=3.5f;
+    float tapW=texTapioca.width*tapScale;
+    float tapH=texTapioca.height*tapScale;
+    float tapX=screenWidth*0.58f;
+    float tapY=(screenHeight-tapH)/2.0f;
+    float reduzirW=tapW*0.20f;
+    float reduzirH=tapH*0.20f;
+    Rectangle tapiocaColisor={tapX+reduzirW/2.0f,tapY+reduzirH/2.0f,tapW-reduzirW,tapH-reduzirH};
+    float ingScale=2.0f;
+    float cW=texCarne.width*ingScale;
+    float cH=texCarne.height*ingScale;
+    float pW=texPeixe.width*ingScale;
+    float pH=texPeixe.height*ingScale;
+    float aW=texAlface.width*ingScale;
+    float aH=texAlface.height*ingScale;
+    float tabulaX=163.0f;
+    float tabulaW=598.0f;
+    float tabulaY=134.0f;
+    float tabulaH=454.0f;
+    float margem=20.0f;
+    float ws[3]={cW,pW,aW};
+    float hs[3]={cH,pH,aH};
+    float posX[3],posY[3];
+    for (int i=0;i<3;i++) {
+        int tentativas=0;
+        int ok=0;
+        while (!ok && tentativas<200) {
+            tentativas++;
+            float rx=tabulaX+margem+(float)(rand()%(int)(tabulaW-ws[i]-margem*2));
+            float ry=tabulaY+margem+(float)(rand()%(int)(tabulaH-hs[i]-margem*2));
+            ok=1;
+            for (int j=0;j<i;j++) {
+                if (rx<posX[j]+ws[j]+margem && rx+ws[i]>posX[j]-margem && ry<posY[j]+hs[j]+margem && ry+hs[i]>posY[j]-margem) {
+                    ok=0;
+                    break;
+                }
+            }
+            if (ok) { posX[i]=rx; posY[i]=ry; }
+        }
+    }
+    Ingrediente ingredientes[3]={
+        {posX[0],posY[0],posX[0],posY[0],cW,cH,"Carne",0,texCarne},
+        {posX[1],posY[1],posX[1],posY[1],pW,pH,"Peixe",0,texPeixe},
+        {posX[2],posY[2],posX[2],posY[2],aW,aH,"Alface",0,texAlface},
     };
-
-    int totalIngredientes=6;
-    int pedido[2];
-    pedido[0]=rand()%totalIngredientes;
-    do {
-        pedido[1]=rand()%totalIngredientes;
-    } while (pedido[1]==pedido[0]);
-
+    int totalIngredientes=3;
+    int pedido=rand()%totalIngredientes;
     int acertos=0;
-
-    Rectangle tapioca={350,220,300,220};
     int arrastando=-1;
-    float dragOffsetX=0.0f;
-    float dragOffsetY=0.0f;
-
+    float dragOffX=0.0f;
+    float dragOffY=0.0f;
     TimerBomba timer;
     InitTimerBomba(&timer,TEMPO_PAVIO,TEMPO_BOMBA_FUSE);
-
-    while (timer.fase==FASE_PAVIO && !errou && acertos<2 && !WindowShouldClose()) {
+    while (timer.fase==FASE_PAVIO && !errou && acertos<1 && !WindowShouldClose()) {
         UpdateTimerBomba(&timer);
-
         Vector2 mouse=GetMousePosition();
-
         if (IsMouseButtonPressed(MOUSE_LEFT_BUTTON)) {
             for (int i=0;i<totalIngredientes;i++) {
                 if (ingredientes[i].colocado) continue;
-                Rectangle rect={ingredientes[i].x,ingredientes[i].y,ingredientes[i].w,ingredientes[i].h};
-                if (CheckCollisionPointRec(mouse,rect)) {
+                Rectangle r={ingredientes[i].x,ingredientes[i].y,ingredientes[i].w,ingredientes[i].h};
+                if (CheckCollisionPointRec(mouse,r)) {
                     arrastando=i;
-                    
-                    dragOffsetX=mouse.x-ingredientes[i].x;
-                    dragOffsetY=mouse.y-ingredientes[i].y;
+                    dragOffX=mouse.x-ingredientes[i].x;
+                    dragOffY=mouse.y-ingredientes[i].y;
                     break;
                 }
             }
         }
-
         if (arrastando>=0) {
-            ingredientes[arrastando].x=mouse.x-dragOffsetX;
-            ingredientes[arrastando].y=mouse.y-dragOffsetY;
+            ingredientes[arrastando].x=mouse.x-dragOffX;
+            ingredientes[arrastando].y=mouse.y-dragOffY;
         }
-
         if (IsMouseButtonReleased(MOUSE_LEFT_BUTTON) && arrastando>=0) {
-            Rectangle rectIng={ingredientes[arrastando].x,ingredientes[arrastando].y,ingredientes[arrastando].w,ingredientes[arrastando].h};
-
-            if (CheckCollisionRecs(rectIng,tapioca)) {
-                int correto=(arrastando==pedido[0]||arrastando==pedido[1]);
-                if (correto) {
+            Rectangle rIng={ingredientes[arrastando].x,ingredientes[arrastando].y,ingredientes[arrastando].w,ingredientes[arrastando].h};
+            if (CheckCollisionRecs(rIng,tapiocaColisor)) {
+                if (arrastando==pedido) {
                     ingredientes[arrastando].colocado=1;
                     acertos++;
                 } else {
@@ -91,83 +122,56 @@ void cozinhaDeTapioca() {
                 ingredientes[arrastando].x=ingredientes[arrastando].origX;
                 ingredientes[arrastando].y=ingredientes[arrastando].origY;
             }
-
             arrastando=-1;
         }
-
         BeginDrawing();
-            ClearBackground((Color){255,245,220,255});
-            DrawRectangle(20,20,300,110,(Color){255,220,150,255});
-
-            DrawRectangleLines(20,20,300,110,BROWN);
-            DrawText("Pedido:",35,35,22,BROWN);
-
-            DrawText(ingredientes[pedido[0]].nome,35,65,20,DARKBROWN);
-            DrawText(ingredientes[pedido[1]].nome,35,90,20,DARKBROWN);
-
-            DrawEllipse((int)(tapioca.x+tapioca.width/2.0f),(int)(tapioca.y+tapioca.height/2.0f),(int)(tapioca.width/2.0f),(int)(tapioca.height/2.0f),(Color){245,240,220,255});
-            DrawEllipseLines((int)(tapioca.x+tapioca.width/2.0f),(int)(tapioca.y+tapioca.height/2.0f),(int)(tapioca.width/2.0f),(int)(tapioca.height/2.0f),LIGHTGRAY);
-            
-            int larguraMassa=MeasureText("Massa",20);
-            DrawText("Massa",(int)(tapioca.x+tapioca.width/2.0f)-larguraMassa/2,(int)(tapioca.y+tapioca.height/2.0f)-10,20,LIGHTGRAY);
-            int offsetColocado=0;
-
-            for (int i=0;i<totalIngredientes;i++) {
-                if (!ingredientes[i].colocado) continue;
-                DrawText(ingredientes[i].nome,(int)(tapioca.x+tapioca.width/2.0f)-MeasureText(ingredientes[i].nome,20)/2,(int)(tapioca.y+tapioca.height/2.0f)+offsetColocado,20,ingredientes[i].cor);
-                offsetColocado+=25;
-            }
+            DrawTexturePro(texFundo,srcFundo,dstFundo,(Vector2){0,0},0.0f,WHITE);
+            DrawRectangleRounded((Rectangle){20,20,260,90},0.2f,4,(Color){0,0,0,140});
+            DrawText("Pedido:",35,32,22,WHITE);
+            DrawText(ingredientes[pedido].nome,35,62,22,YELLOW);
+            DrawTexturePro(texTapioca,(Rectangle){0,0,(float)texTapioca.width,(float)texTapioca.height},(Rectangle){tapX,tapY,tapW,tapH},(Vector2){0,0},0.0f,WHITE);
             for (int i=0;i<totalIngredientes;i++) {
                 if (ingredientes[i].colocado) continue;
                 if (i==arrastando) continue;
-                DrawRectangleRounded((Rectangle){ingredientes[i].x,ingredientes[i].y,ingredientes[i].w,ingredientes[i].h},0.3f,4,ingredientes[i].cor);
-                DrawRectangleRoundedLines((Rectangle){ingredientes[i].x,ingredientes[i].y,ingredientes[i].w,ingredientes[i].h},0.3f,4,DARKGRAY);
-                int larguraNome=MeasureText(ingredientes[i].nome,18);
-                DrawText(ingredientes[i].nome,(int)(ingredientes[i].x+ingredientes[i].w/2.0f)-larguraNome/2,(int)(ingredientes[i].y+ingredientes[i].h/2.0f)-9,18,BLACK);
+                DrawIngrediente(&ingredientes[i]);
             }
-            if (arrastando>=0) {
-                DrawRectangleRounded((Rectangle){ingredientes[arrastando].x,ingredientes[arrastando].y,ingredientes[arrastando].w,ingredientes[arrastando].h},0.3f,4,ingredientes[arrastando].cor);
-                DrawRectangleRoundedLines((Rectangle){ingredientes[arrastando].x,ingredientes[arrastando].y,ingredientes[arrastando].w,ingredientes[arrastando].h},0.3f,4,DARKGRAY);
-                int larguraNome=MeasureText(ingredientes[arrastando].nome,18);
-                DrawText(ingredientes[arrastando].nome,(int)(ingredientes[arrastando].x+ingredientes[arrastando].w/2.0f)-larguraNome/2,(int)(ingredientes[arrastando].y+ingredientes[arrastando].h/2.0f)-9,18,BLACK);
-            }
+            if (arrastando>=0) DrawIngrediente(&ingredientes[arrastando]);
             DrawTimerBomba(&timer,screenWidth,screenHeight);
         EndDrawing();
     }
-
-    if (acertos>=2) {
-        flag=1;
-    }
+    if (acertos>=1) flag=1;
     if (!errou && !flag) {
         while (!TimerTerminou(&timer) && !WindowShouldClose()) {
             UpdateTimerBomba(&timer);
             BeginDrawing();
-                ClearBackground((Color){255,245,220,255});
+                DrawTexturePro(texFundo,srcFundo,dstFundo,(Vector2){0,0},0.0f,WHITE);
                 DrawTimerBomba(&timer,screenWidth,screenHeight);
             EndDrawing();
         }
     }
-
     if (flag) {
         while (frames>0 && !WindowShouldClose()) {
             frames--;
             BeginDrawing();
-                ClearBackground((Color){255,245,220,255});
-                int tamanhoFonte=40;
-                int larguraTexto=MeasureText("Vitoria",tamanhoFonte);
-                DrawText("Vitoria",(screenWidth/2)-(larguraTexto/2),(screenHeight/2)-(tamanhoFonte/2),tamanhoFonte,GREEN);
+                DrawTexturePro(texFundo,srcFundo,dstFundo,(Vector2){0,0},0.0f,WHITE);
+                int ts=40,lw=MeasureText("Vitoria",ts);
+                DrawText("Vitoria",(screenWidth/2)-(lw/2),(screenHeight/2)-(ts/2),ts,GREEN);
             EndDrawing();
         }
     } else {
         while (frames>0 && !WindowShouldClose()) {
             frames--;
             BeginDrawing();
-                ClearBackground((Color){255,245,220,255});
-                int tamanhoFonte=40;
-                int larguraTexto=MeasureText("Derrota",tamanhoFonte);
-                DrawText("Derrota",(screenWidth/2)-(larguraTexto/2),(screenHeight/2)-(tamanhoFonte/2),tamanhoFonte,RED);
+                DrawTexturePro(texFundo,srcFundo,dstFundo,(Vector2){0,0},0.0f,WHITE);
+                int ts=40,lw=MeasureText("Derrota",ts);
+                DrawText("Derrota",(screenWidth/2)-(lw/2),(screenHeight/2)-(ts/2),ts,RED);
             EndDrawing();
         }
     }
+    UnloadTexture(texFundo);
+    UnloadTexture(texTapioca);
+    UnloadTexture(texCarne);
+    UnloadTexture(texPeixe);
+    UnloadTexture(texAlface);
     UnloadTimerBomba(&timer);
 }
